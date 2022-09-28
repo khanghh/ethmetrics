39b378d974da0e63e910b713b391284789cc3c64
Khang Hoang <hoanghongkhang@gmail.com>
2022-09-28 18:27:02 +0700

    record block.timestamp

--- internal/core/collector/block.go
+++ internal/core/collector/block.go
@@ -16,12 +16,14 @@ func (m *BlockMetrics) Setup(ctx *core.Ctx, registry metrics.Registry) error {
 
 func (m *BlockMetrics) Collect(ctx *core.Ctx) {
 	blockHeadGauge := metrics.GetOrRegisterGauge("eth/block.number", ctx.Registry)
+	blockTimestampGauge := metrics.GetOrRegisterGauge("eth/block.timestamp", ctx.Registry)
 	blockSizeGauge := metrics.GetOrRegisterGaugeFloat64("eth/block.size", ctx.Registry)
 	blockGasUsedGauge := metrics.GetOrRegisterGauge("eth/block.gasused", ctx.Registry)
 	blockGasLimitGauge := metrics.GetOrRegisterGauge("eth/block.gaslimit", ctx.Registry)
 	blockTxnCountGauge := metrics.GetOrRegisterGauge("eth/block.txncount", ctx.Registry)
 
 	blockHeadGauge.Update(int64(ctx.LatestBlock.NumberU64()))
+	blockTimestampGauge.Update(int64(ctx.LatestBlock.Time()))
 	blockSizeGauge.Update(float64(ctx.LatestBlock.Size()))
 	blockGasUsedGauge.Update(int64(ctx.LatestBlock.GasUsed()))
 	blockGasLimitGauge.Update(int64(ctx.LatestBlock.GasLimit()))
